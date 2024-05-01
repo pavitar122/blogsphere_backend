92cd0e986c4d7bf58dd4530c1c30083605b1d3a3
pavitar122 <pavitar3335@gmail.com>
2024-05-01 21:50:49 +0530

    first commit

--- db/config.js
+++ db/config.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 try {
-    mongoose.connect(process.env.MONGO_URL)
+    mongoose.connect("mongodb+srv://pavitar3335:YYoGZrKHt4ZWwrDJ@cluster0.xrgjw4k.mongodb.net/?retryWrites=true&w=majority")
     console.log("connected")
 }catch(err){
     console.log(err)
